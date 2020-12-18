98987add4477112f9894b3727f387c754c4d00bc
Raza <razaattari2468@gmial.com>
2020-12-18 16:39:57 +0500

    change

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var express = require("express")
 var server = express()
 
 
-var PORT = process.env.PORT || 3001
+var PORT = process.env.PORT || 3001;
+
 server.get("/", (req,res,next)=>{
 console.log("runinig===>biyani");
     res.send("Hellow i will come biryani $ Home $")
@@ -14,6 +15,6 @@ server.get("/about", (req,res,next)=>{
     })
     
 
-server.listen(PORT || 3001,()=>{
+server.listen(PORT,()=>{
     console.log('server is running' +PORT);
 })
